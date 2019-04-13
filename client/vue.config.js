ebf514a9de2edfc34d1def8668cf3f62c808fa2d
Chillen <josh.jenkin@live.com>
2019-04-13 12:03:02 -0600

    resolve path, bootstrap

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 
 module.exports = {
-    baseUrl: path.resolve('/'),
     outputDir: path.resolve(__dirname, "../public/"),
     devServer: {
         proxy: 'http://localhost:3000'
