b55fb70cfc79492812f1e5edbbed76b20b1b7369
Jamison Gordon <jgordon@uhin.org>
2019-04-18 19:19:04 -0600

    Added Josh's changes for the register page

--- src/components/register.vue
+++ src/components/register.vue
@@ -1,20 +1,6 @@
 <template>
   <div class="container">
     <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group
-        id="input-group-1"
-        label="Email address:"
-        label-for="input-1"
-        description="We'll never share your email with anyone else."
-      >
-        <b-form-input
-          id="input-1"
-          v-model="form.email"
-          type="email"
-          required
-          placeholder="Enter email"
-        ></b-form-input>
-      </b-form-group>
 
       <b-form-group id="input-group-2" label="User Name:" label-for="input-2">
         <b-form-input
@@ -34,8 +20,8 @@
         ></b-form-input>
       </b-form-group>
 
-      <b-button type="submit" variant="success">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
+      <b-button type="submit" style="background-color: rgba(31, 168, 74, 1);">Submit</b-button>
+      <b-button type="reset" style="background-color: rgba(168, 50, 49, 1);">Reset</b-button>
     </b-form>
     <b-card class="mt-3" header="Form Data Result">
       <pre class="m-0">{{ form }}</pre>
@@ -44,12 +30,12 @@
 </template>
 
 <script>
+
   export default {
     // name: 'register',
     data() {
       return {
         form: {
-          email: '',
           name: '',
           password: ''
         },
@@ -60,6 +46,7 @@
       onSubmit(evt) {
         evt.preventDefault()
         alert(JSON.stringify(this.form))
+        // put function to add user data to database
       },
       onReset(evt) {
         evt.preventDefault()
