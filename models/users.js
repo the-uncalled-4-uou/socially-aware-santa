4cc21b09e065fb1ea385d2a517de17e217be32ca
lmccray <lmccray@uhin.org>
2019-04-20 10:19:22 -0600

    adding data model

--- models/users.js
+++ models/users.js
@@ -5,6 +5,33 @@ const saltRounds = 10;
 //Define a schema
 const Schema = mongoose.Schema;
 
+const RuleSchema = new Schema({
+    rulename: {
+        type: String,
+        trim:true
+    },
+    rule: {
+        type: String,
+        trim: true
+    }
+})
+
+const NameSchema = new Schema({
+    personname: {
+        type: String,
+        trim: true
+    }
+})
+
+const ListsSchema = new Schema({
+    listname: {
+        type: String,
+        trim: true
+    },
+    names: [NameSchema],
+    rules: [RuleSchema]
+})
+
 const UserSchema = new Schema({
     username: {
         type: String,
@@ -15,7 +42,8 @@ const UserSchema = new Schema({
         type: String,
         trim: true,
         required: true
-    }
+    },
+    lists: [ListsSchema]
 });
 
 UserSchema.pre('save', function(next){
