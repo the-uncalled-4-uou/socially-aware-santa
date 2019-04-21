8f47a8878c5f4c78f7a2f84d6dacb40475b067de
Codey <codeymcdaniel@gmail.com>
2019-04-21 08:09:59 -0600

    added api routes to apiRoutes file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,54 @@
-const db = require('../models');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
+const db = require("../models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports = function(app) {
-
-    app.get("/api/users", function (req, res) {
-        console.log("got here");
-        jwt.verify(req.headers['x-access-token'], req.app.get('secretKey'), function (err, decoded) {
-            if (err) {
-                res.json({ status: "error", message: err.message, data: null });
-            } else {
-
-                // add user id to request
-                req.body.userId = decoded.id;
-                res.json({ userid: req.body.userId })
+  app.get("/api/user", function(req, res) {
+    db.Users.updateOne(
+      { username: "codey@email.com" },
+      {
+        $push: {
+          lists: {
+            $each: {
+              name: "friends",
+              people: ["john", "jane", "jim", "jean"],
+              cannotMatch: [["jim", "jean"], ["john", "jane"]]
             }
-        });
-    });
+          }
+        }
+      }
+    )
+      .then(function(data) {
+        res.send(data);
+      })
+      .catch(function(err) {
+        res.send(err.message);
+      });
+    // jwt.verify(
+    //   req.headers["x-access-token"],
+    //   req.app.get("secretKey"),
+    //   function(err, decoded) {
+    //     if (err) {
+    //       res.json({ status: "error", message: err.message, data: null });
+    //     } else {
+    //       // add user id to request
+    //       req.body.userId = decoded.id;
+    //       res.json({ userid: req.body.userId });
+    //     }
+    //   }
+    // );
+    // db.Users.findOne({ username: "codeycsm@email.com" }).then(function(data) {
+    //   res.send(data);
+    // });
+  });
 
-    app.post("/api/lists", function (req, res) {
-        db.Users.create(req.body)
-        .then(function(dbuser) {
-            res.json(dbuser);
-
-        })
-        .catch(function(err) {
-            res.json(err)
-        })
-
-    })
-
-
-}
+  app.post("/api/lists", function(req, res) {
+    db.Users.create(req.body)
+      .then(function(dbuser) {
+        res.json(dbuser);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,36 +1,43 @@
-const db = require('../models')
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const db = require("../models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports = function(app) {
-
-
-    app.post("/api/register", function (req, res) {
-        console.log("got here")
-        console.log(req.body)
-        db.Users.create(req.body).then(function (dbExample) {
-            res.json(dbExample);
-        });
+  app.post("/api/register", function(req, res) {
+    console.log("got here");
+    console.log(req.body);
+    db.Users.create(req.body).then(function(dbExample) {
+      res.json(dbExample);
     });
-    app.post("/api/authenticate", function (req, res) {
-        db.Users.findOne({ username: req.body.username }, function (err, userInfo) {
-            if (err) {
-                next(err);
-            } else {
+  });
+  app.post("/api/authenticate", function(req, res) {
+    db.Users.findOne({ username: req.body.username }, function(err, userInfo) {
+      if (err) {
+        next(err);
+      } else {
+        if (
+          userInfo != null &&
+          bcrypt.compareSync(req.body.password, userInfo.password)
+        ) {
+          const token = jwt.sign(
+            { id: userInfo._id },
+            req.app.get("secretKey"),
+            { expiresIn: "1h" }
+          );
 
-                if (userInfo != null && bcrypt.compareSync(req.body.password, userInfo.password)) {
-
-                    const token = jwt.sign({ id: userInfo._id }, req.app.get('secretKey'), { expiresIn: '1h' });
-
-                    res.json({ status: "success", message: "user found!!!", data: { user: userInfo, token: token } });
-
-                } else {
-
-                    res.json({ status: "error", message: "Invalid email/password!!!", data: null });
-
-                }
-            }
-        });
-    })
-
-}
+          res.json({
+            status: "success",
+            message: "user found!!!",
+            data: { user: userInfo, token: token }
+          });
+        } else {
+          res.json({
+            status: "error",
+            message: "Invalid email/password!!!",
+            data: null
+          });
+        }
+      }
+    });
+  });
+};
