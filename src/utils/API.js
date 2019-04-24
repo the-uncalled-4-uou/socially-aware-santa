e9a1f3c78672b05cdaf192b00800364648d6a9b1
Jamison Gordon <jgordon@uhin.org>
2019-04-23 21:50:45 -0600

    Fixed API having duplicates

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,32 +3,7 @@ import axios from "axios";
 
 export default {
 
-<<<<<<< HEAD
   // Gets user info
-  login (data) {
-    return axios.post("/api/authenticate", data);
-  },
-  // Saves a new user to the database
-  registerUser (userData) {
-    return axios.post("/api/register", userData);
-  },
-  getUserLists (jwt) {
-      return axios({
-          method: 'get',
-          url: '/api/lists',
-          headers: {'x-access-token': jwt}
-      });
-  },
-  addUserList(jwt, data) {
-    return axios({
-      method: 'post',
-      url: '/api/lists',
-      headers: {'x-access-token': jwt},
-      data: data
-    })
-  }
-=======
-    // Log user in
     login (data) {
         return axios.post("/api/authenticate", data);
     },
@@ -49,7 +24,13 @@ export default {
             url: '/api/lists',
             headers: {'x-access-token': jwt}
         });
+    },
+    addUserList(jwt, data) {
+        return axios({
+            method: 'post',
+            url: '/api/lists',
+            headers: {'x-access-token': jwt},
+            data: data
+        })
     }
-
->>>>>>> master
 };
