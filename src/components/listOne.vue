53e3042c0fc40f22df309f1dfa48a00a070cf311
lmccray <lmccray@uhin.org>
2019-04-27 02:54:35 -0600

    working list generation

--- src/components/listOne.vue
+++ src/components/listOne.vue
@@ -7,7 +7,8 @@
 
             <!-- Element to collapse -->
             <b-collapse v-bind:id="name._id"> Give Rules:
-                <div v-for="giverule in name.giverules" :key="giveruledata.indexOf(giverule)"> {{ giverule.nameid }}
+                <div v-for="giverule in name.giverules" :key="giveruledata.indexOf(giverule)"> {{
+                    friendlyname(giverule.nameid) }}
                     <b-button @click="deleteGiveRule(name._id, giverule._id)">Delete</b-button>
                     <br>
 
@@ -21,11 +22,11 @@
                 <br>
                 Recieve Rules:
                 <div v-for="recieverule in name.receiverules" :key="recieveruledata.indexOf(recieverule)">
-                    {{ recieverule.nameid }}
+                    {{ friendlyname(recieverule.nameid) }}
                     <b-button @click="deleteReceiveRule(name._id, recieverule._id)">Delete</b-button>
                 </div>
                 <b-dropdown class="m-md-2" text="Add Rule">
-                    <b-dropdown-item @click="addRecieverule(name._id, dropname._id)" v-for="dropname in resdata"
+                    <b-dropdown-item  @click="addRecieverule(name._id, dropname._id)" v-for="dropname in resdata"
                                      :key="resdata.indexOf(dropname)">{{ dropname.personname }}
                     </b-dropdown-item>
                 </b-dropdown>
@@ -47,7 +48,9 @@
             <br>
             <br>
 
-            <div v-for="match in fulllist.matchedlist">{{ match }}</div>
+            <div v-for="match in matchedlist" :key="matchedlist.indexOf(match)">{{ match.from }} gives to {{ match.to
+                }}
+            </div>
         </b-card>
     </div>
 </template>
@@ -69,10 +72,8 @@
             };
         },
         created() {
-            console.log(this.listpage);
             API.getListNames(localStorage.getItem('jwt'), this.listpage).then(res => {
                 this.resdata = res.data;
-                console.log(this.resdata);
             });
             API.getList(localStorage.getItem('jwt'), this.listpage).then(res => {
                 this.fulllist = res.data;
@@ -133,42 +134,50 @@
                 return a;
             },
             checkmatch() {
-                let left = []
-                let right = []
-                let matches = []
-                let namelength = this.resdata.length
+                let left = [];
+                let right = [];
+                let matches = [];
+                //this.matchedlist = []
+                let namelength = this.resdata.length;
                 for (var i = 0; i < this.resdata.length; i++) {
                     left.push(this.resdata[i]._id);
                     right.push(this.resdata[i]._id);
                 }
-                this.shuffle(left)
-                this.shuffle(right)
-                let loopdone = false
-                while(!loopdone) {
-                    console.log("asdf")
-                    if (this.matchcheck(left[0],right[0])) {
-                        matches.push({ from: left[0], to: right[0] })
-                        left.splice(0, 1);
-                        right.splice(0, 1);
-                        if (matches.length === namelength) {
-                            loopdone = true
-                            break;
-                        }
-                    }
-                    else {
-                        this.shuffle(left)
-                        this.shuffle(right)
-                        if (matches.length === namelength) {
-                            loopdone = true
-                            break;
-                        }
-                    }
-                    if (matches.length === namelength) {
-                        loopdone = true
+                this.shuffle(left);
+                this.shuffle(right);
+                let loopdone = false;
+                while (!loopdone) {
+                    //debugger;
+                    let sucessful = this.matchcheck(left[0], right[0]);
+                    if (!sucessful && matches.length === namelength - 1) {
+                        loopdone = true;
+                        this.matchedlist = [];
+                        this.checkmatch();
                         break;
                     }
+                    if (sucessful) {
+                        matches.push({ from: left[0], to: right[0] });
+                        left.splice(0, 1);
+                        right.splice(0, 1);
+                        this.shuffle(left);
+                        this.shuffle(right);
+                    } else {
+                        if (left[0] === right[0]) {
+                            loopdone = true;
+                            this.matchedlist = [];
+                            this.checkmatch();
+                            break;
+                        }
+                        this.shuffle(left);
+                        this.shuffle(right);
+                    }
+                    if (matches.length === namelength) {
+                        loopdone = true;
+                        //this.matchedlist = matches
+                        this.fixnames(matches);
+                    }
                 }
-                console.log(matches)
+
 
             },
             matchcheck(id1, id2) {
@@ -176,22 +185,36 @@
                 var indexofid2 = this.resdata.findIndex(i => i._id === id2);
                 var findindex = this.resdata[indexofid1].giverules.findIndex(i => i.nameid === id2);
                 var findindex4 = this.resdata[indexofid2].receiverules.findIndex(i => i.nameid === id1);
-                if(id1 === id2) {
-                    return false
-                }
-                else if (findindex > -1) {
-                    return false
-                }
-                else if (findindex4 > -1) {
-                    return false
-                }
-                else {
-                    return true
+                if (id1 === id2) {
+                    return false;
+                } else if (findindex > -1) {
+                    return false;
+                } else if (findindex4 > -1) {
+                    return false;
+                } else {
+                    return true;
                 }
             },
             calculateList() {
-                this.checkmatch()
+                this.checkmatch();
+            },
+            fixnames(matches) {
+                var newarray = [];
+                for (var i = 0; i < matches.length; i++) {
+                    let fromindex = this.resdata.findIndex(j => j._id === matches[i].from);
+                    let toindex = this.resdata.findIndex(j => j._id === matches[i].to);
+                    let fromname = this.resdata[fromindex].personname;
+                    let toname = this.resdata[toindex].personname;
+                    newarray.push({ from: fromname, to: toname });
+                }
+                this.matchedlist = newarray;
+            },
+            friendlyname(nameid) {
+                let nameindex = this.resdata.findIndex(j => j._id === nameid);
+                let name = this.resdata[nameindex].personname;
+                return name;
             }
+
         }
     };
 
